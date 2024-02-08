676cb1acfc38bdf5ae403d3fa785276b5e0d3aba
akash8347 <123393293+akash8347@users.noreply.github.com>
2024-02-08 15:23:11 +0530

    Update route.js
    added revalidate path

--- app/api/blog/getposts/route.js
+++ app/api/blog/getposts/route.js
@@ -1,6 +1,7 @@
 // api/posts.js
 import connectDB from "@/app/lib/mongocon";
 import mongoose from "mongoose";
+import { revalidatePath } from 'next/cache';
 // import Post from "@/app/lib/schemas/post";// Update the path accordingly
 // var Post = mongoose.model('testpost')
 var post=require('../../../lib/schemas/post')
@@ -8,6 +9,7 @@ export async function GET(request) {
     try {
        await connectDB()
         const posts = await post.find();
+        revalidatePath("/blog");
         return Response.json(posts);
     } catch (error) {
         console.error(error);
