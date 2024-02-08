757227575982bc8c07d8ff665fbcad5f9c69e40b
akash8347 <123393293+akash8347@users.noreply.github.com>
2024-02-08 12:25:27 +0530

    Update page.jsx
    { next: { revalidate: 3600 } }

--- app/(pages)/blog/page.jsx
+++ app/(pages)/blog/page.jsx
@@ -14,7 +14,7 @@ const Page = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await fetch("/api/blog/getposts",  {cache:"no-cache"},{
+                const res = await fetch("/api/blog/getposts", { next: { revalidate: 3600 } },{
                     method: 'GET',
                 });
 
