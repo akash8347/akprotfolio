8fa22839fdaaed59ea6b3f6820497bb90d79d639
akash8347 <123393293+akash8347@users.noreply.github.com>
2024-02-08 12:29:11 +0530

    Update page.jsx
    removed cache

--- app/(pages)/blog/page.jsx
+++ app/(pages)/blog/page.jsx
@@ -14,7 +14,7 @@ const Page = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const res = await fetch("/api/blog/getposts", { next: { revalidate: 1 } },{
+                const res = await fetch("/api/blog/getposts", {
                     method: 'GET',
                 });
 
