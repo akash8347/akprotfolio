49a585d6bf7a669e61eca46d1f04d7616987a450
akash8347 <123393293+akash8347@users.noreply.github.com>
2024-02-16 12:26:50 +0530

    Update projdata.js
    removed extra projdata

--- app/lib/projdata.js
+++ app/lib/projdata.js
@@ -7,33 +7,7 @@ const data = [{
     icon: ['SiMysql', 'FaReact'],
     niche: ['ReactJS', 'NodeJs', 'MySQL']
   },
-  {
-    id:2,
-    imgsrc: "projectecom.png",
-    title: "Increasing the title",
-    description: `test is made by me site using ReactJS, NodeJS, MySQL. Added added user and admin modules. 
-      Added functionality for admin to add,remove,update products`,
-    icon: ['FaReact'],
-    niche: ['ReactJS', 'MySQL', 'NodeJS']
-  },
-  {
-    id:3,
-    imgsrc: "projectecom.png",
-    title: "satish ka project",
-    description: `test is made by me site using ReactJS, NodeJS, MySQL. Added added user and admin modules. 
-    Added functionality for admin to add,remove,update products`,
-    icon: ['FaReact'],
-    niche: ['react', 'mahesh', 'virat']
-  },
-  {
-    id:4,
-    imgsrc: "projectecom.png",
-    title: "satish ka project",
-    description: `test is made by me site using ReactJS, NodeJS, MySQL. Added added user and admin modules. 
-    Added functionality for admin to add,remove,update products`,
-    icon: ['FaReact'],
-    niche: ['jayesh', 'mahesh', 'virat']
-  },
+   
  
 
 
