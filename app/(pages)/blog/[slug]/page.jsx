c1b1b393d7388249a3d6f36c00d5052cb262cc16
akash8347 <akashgohil.av@gmail.com>
2024-06-09 17:46:00 +0530

    new commit

--- app/(pages)/blog/[slug]/page.jsx
+++ app/(pages)/blog/[slug]/page.jsx
@@ -1,40 +1,55 @@
-"use client"
-import Image from "next/image";
-import { useState, useEffect } from "react";
-import Spinner1 from "@/app/Components1/Spinner1";
-import Head from "next/head";
+import Head from 'next/head';
+import Image from 'next/image';
 import parse from 'html-react-parser';
 
-export default function Page({ params }) {
-  const [postData, setPostData] = useState(null);
-  const [error, setError] = useState(null);
+// Fetch post data based on the slug
 
 
-  useEffect(() => {
-    const fetchPostData = async () => {
-      try {
-        const res = await fetch(`/api/blog/singlepost/${params.slug}`);
-        if (!res.ok) {
-          setError('Failed to fetch post data');
 
-        }
-        const postData = await res.json();
-        console.log(postData.content);
-        setPostData(postData);
-      } catch (error) {
-        console.error('Error fetching post data:', error);
-      }
-    };
+const fetchPostData = async (params) => {
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+    const res = await fetch(`${baseUrl}/api/blog/singlepost/${params.slug}`);
+    if (res.ok) {
+      return await res.json();
+    } else {
+      return { error: `Failed to fetch post data with status: ${res.status}` };
+    }
+  } catch (error) {
+    console.error('Error fetching post data:', error);
+    return { error: 'An error occurred while fetching the post data' };
+  }
+};
 
-    fetchPostData();
-  }, [params.slug]);
+
+export function generateMetadata({params}){
+return{
+  title:params.slug
+}
+}
+
+export default async function Page({ params }) {
+  const postData = await fetchPostData(params);
+
+  if (postData.error) {
+    return (
+      <>
+        <Head>
+          <title>Error - {params.slug.replace(/-/g, ' ')}</title>
+        </Head>
+        <div className="h-screen w-full flex justify-center items-center">
+          <p>{postData.error}</p>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <Head>
-        <title>{params.slug.replace(/-/g, ' ')}</title>
+        <title>{postData.title} | Akash Gohil</title>
       </Head>
-      {!error && postData ? (
+      {postData ? (
         <div className="min-h-screen mt-10 mx-auto w-[93%] md:w-[60%]">
           <div className="heading">
             <div className="title font-semibold text-xl md:text-3xl">
@@ -58,7 +73,7 @@ export default function Page({ params }) {
         </div>
       ) : (
         <div className="h-screen w-full flex justify-center items-center">
-       {error?(<p>something gone wring </p>):(<Spinner1/>)}
+          <p>Something went wrong</p>
         </div>
       )}
     </>
